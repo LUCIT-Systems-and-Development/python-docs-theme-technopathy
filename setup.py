98be182ce47793c962b13625dd290535aacc60ab
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-11-04 12:59:32 +0100

    release 0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with io.open('README.rst', encoding='utf-8') as readme_file:
 
 setup(
     name='python-docs-theme-technopathy',
-    version='0.2.14',
+    version='0.3.0',
     description='A full width sphinx template for github pages based on python-docs-theme',
     long_description=long_description,
     author='Oliver Zehentleitner',

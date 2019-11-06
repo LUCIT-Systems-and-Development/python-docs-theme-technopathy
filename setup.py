a2014e771f14b46858e4e0eb8737a50fd692917e
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-11-06 15:08:33 +0100

    github stats

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ from setuptools import setup
 
 with io.open('README.rst', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
-
 setup(
     name='python-docs-theme-technopathy',
-    version='0.4.3',
-    description='A full width sphinx template for github pages based on python-docs-theme',
+    version='0.4.4',
+    description='A full width sphinx theme for github pages based on python-docs-theme',
     long_description=long_description,
     author='Oliver Zehentleitner',
     url='https://about.me/oliver-zehentleitner',
@@ -27,6 +26,9 @@ setup(
 
     classifiers=[
         'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Framework :: Sphinx',
+        'Framework :: Sphinx :: Theme',
         'License :: OSI Approved :: Python Software Foundation License',
         'Operating System :: OS Independent',
         'Topic :: Documentation',

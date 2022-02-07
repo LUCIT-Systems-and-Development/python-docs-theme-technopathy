3ecc869e2e3d9c447919b2ea21493e6dad32e0ba
oliver <oliver@zehentleitner.co>
2022-02-07 22:49:47 +0100

    0.7.30

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with io.open('README.rst', encoding='utf-8') as readme_file:
 
 setup(
     name='python-docs-theme-technopathy',
-    version='0.7.29',
+    version='0.7.30',
     description='A responsive sphinx theme for github pages based on python-docs-theme',
     long_description=long_description,
     author='Oliver Zehentleitner',

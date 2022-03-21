31afe5687a24e8a2cf481865de64c748c5146488
oliver <oliver@zehentleitner.co>
2022-03-21 16:31:45 +0100

    badges

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import io
 from setuptools import setup
 
-with io.open('README.rst', encoding='utf-8') as readme_file:
+with io.open('README.md', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 setup(
